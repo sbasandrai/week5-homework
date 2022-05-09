4b31e287a0a45368563638775cc5141ed3d48a90
Sachin <sachin.basandrai@mwphillips.co.uk>
2022-05-09 19:50:40 +0100

    function to change timeblock colour for past/present/future

--- 02-Homework/Develop/js.js
+++ 02-Homework/Develop/js.js
@@ -5,3 +5,15 @@ $("#currentDay").text(today.format("MMM Do, YYYY"));
 
 var timeBlock = $(".hour");
 var now = parseInt(moment().format("H"));
+
+// function for each timeblock to highlight in different colour whether it is present/past/future"
+$.each(timeBlock, function (i, hour) {
+  var hourId = parseInt($(this).attr("id"));
+  if (hourId === now) {
+    $(this).next().addClass("present");
+  } else if (hourId < now) {
+    $(this).next().addClass("past");
+  } else if (hourId > now) {
+    $(this).next().addClass("future");
+  }
+});
